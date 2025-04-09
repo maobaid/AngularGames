759b99b4633a5ccf1e7eda0efe77e2315a52e2d6
maobaid <maarijalobaid@gmail.com>
2025-04-09 11:57:17 +0300

    added desing, functionality, and assets

--- src/app/kirby-clicks/kirby-clicks.component.ts
+++ src/app/kirby-clicks/kirby-clicks.component.ts
@@ -5,8 +5,42 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './kirby-clicks.component.html',
-  styleUrl: './kirby-clicks.component.css'
+  styleUrl: './kirby-clicks.component.css',
 })
 export class KirbyClicksComponent {
+  gameStarted = false;
+  coins: number = 0;
+  clicks: number = 0;
+  kirbyClicked = false;
+  flowerCliked = false;
+  showFlower = false;
 
+  flowerCount: number = 0;
+
+  startGame() {
+    this.gameStarted = true;
+  }
+
+  onKirbyClick() {
+    this.kirbyClicked = true;
+    this.clicks++;
+    if (this.clicks < 10) {
+      this.coins = this.clicks;
+    } else if (this.clicks % 10 == 0) {
+      //show flower
+      this.showFlower = true;
+    } else {
+      this.coins += 2;
+    }
+    setTimeout(() => {
+      this.kirbyClicked = false;
+    }, 100);
+  }
+
+  onFlowerClick() {
+    this.flowerCount++;
+    this.showFlower = false;
+    this.coins -= 10;
+    this.coins += this.clicks * 2;
+  }
 }
